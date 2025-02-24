35f04a1ea5e7ab0b6333a9d1625c8801163d8208
oV1t <102901228+oV1t@users.noreply.github.com>
2025-02-24 17:44:31 +0200

    Update urls.py

--- mainapp/store/urls.py
+++ mainapp/store/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
     path('product/<int:pk>/edit/', views.product_update, name='product_update'),
     path('product/<int:pk>/delete/', views.product_delete, name='product_delete'),
-
 ]
